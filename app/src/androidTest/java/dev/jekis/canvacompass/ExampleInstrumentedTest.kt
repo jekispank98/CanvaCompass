e3f610a86af2a8cf464a3c6f703edf61d577edcd
Jekis <jekispank98@gmail.com>
2026-07-11 14:41:42 +0300

    Set up ci script to run tests due PR to master

--- app/src/androidTest/java/dev/jekis/canvacompass/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/dev/jekis/canvacompass/ExampleInstrumentedTest.kt
@@ -28,7 +28,6 @@ class ExampleInstrumentedTest {
 
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("dev.jekis.canvacompass", appContext.packageName)
     }
